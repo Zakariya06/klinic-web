90263e3bf5930e6253f077d2c06dbf3e55410b84
Zakariya06 <zakariyakhanamar00@gmail.com>
2026-01-31 23:55:56 +0500

    update client

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,20 +1,18 @@
-import axios from "axios"; 
+import axios from "axios";
 export const baseUrl = import.meta.env.PROD
   ? import.meta.env.VITE_TEST_BE_URL ||
     "https://klinic-api-467097446026.europe-west1.run.app"
   : "";
 
-const apiClient = axios.create({ baseURL: baseUrl });
+const apiClient = axios.create({ baseURL: import.meta.env.VITE_TEST_BE_URL });
 
-apiClient.interceptors.request.use((config) => { 
-    const token = localStorage.getItem('token')
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
- 
- 
 
 export default apiClient;

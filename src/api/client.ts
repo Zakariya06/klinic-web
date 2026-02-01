311f255e1413640f5dc50a7221c0af7be450ab64
Zakariya06 <zakariyakhanamar00@gmail.com>
2026-02-01 13:43:15 +0500

    add

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,17 +1,22 @@
 import axios from "axios";
-export const baseUrl = import.meta.env.PROD
-  ? import.meta.env.VITE_TEST_BE_URL ||
-    "https://klinic-api-467097446026.europe-west1.run.app"
-  : "";
 
-const apiClient = axios.create({ baseURL: import.meta.env.VITE_TEST_BE_URL });
+const baseURL =
+  import.meta.env.VITE_TEST_BE_URL ||
+  "https://klinic-api-467097446026.europe-west1.run.app";
+
+const apiClient = axios.create({ baseURL });
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+  console.log("this is local token", token);
   if (token) {
     config.headers = config.headers ?? {};
     config.headers.Authorization = `Bearer ${token}`;
+    console.log("Token send successfully ........");
+  } else {
+    console.log("Token Failed ........");
   }
+
   return config;
 });
 
